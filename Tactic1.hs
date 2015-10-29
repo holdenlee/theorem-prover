e852d23884180a7ef40142c440650cccce9ce759
Holden Lee <holdenl@princeton.edu>
2015-10-29 10:43:24 -0400

    rewrite Tactic using ListT

--- Tactic1.hs
+++ Tactic1.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 {-# OPTIONS
  
  -XMultiParamTypeClasses
@@ -8,37 +10,21 @@
  -XPolyKinds
 #-}
 
-module Tactic where
+module Tactic1 where
 import Control.Monad
 import Control.Monad.Reader
 import Data.Traversable
+import Control.Monad.Trans.List
+import Data.Monoid
 
-newtype Tactic c w = Tactic (Reader c [w])
---maybe I can do this with ListT? But ListT is funky.
-
-_tactic :: Tactic c w -> Reader c [w]
-_tactic (Tactic t) = t
-
-instance Functor (Tactic c) where
-    fmap f (Tactic r) = Tactic $ fmap (map f) r
-
-instance Applicative (Tactic c) where
-    pure x = Tactic $ return [x]
-    (Tactic f) <*> (Tactic x) = Tactic $ do
-                                  fs <- f
-                                  li <- x
-                                  return (fs <*> li)
-
-instance Monad (Tactic c) where
-    (Tactic x) >>= f = Tactic $ do
-                            li <- x
-                            fmap concat $ sequenceA (fmap (_tactic . f) li)
+--Reader is commutative
+type Tactic c w = ListT (Reader c) w
 
 (.&) :: (w -> Tactic c x) -> (x -> Tactic c y) -> (w -> Tactic c y)
 (.&) = (>=>)
 
 (.|) :: (w -> Tactic c x) -> (w -> Tactic c x) -> (w -> Tactic c x)
-(.|) f g x = Tactic $ (++) <$> (_tactic $ f x) <*> (_tactic $ g x)
+(.|) f g x = ListT $ (++) <$> (runListT $ f x) <*> (runListT $ g x)
 
 try :: (w -> Tactic c w) -> (w -> Tactic c w)
 try f = f .| return
