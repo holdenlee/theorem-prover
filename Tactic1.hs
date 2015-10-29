4555831fb9e3e053b1dda135d5ce06ae46f18180
Holden Lee <holdenl@princeton.edu>
2015-10-29 10:50:05 -0400

    ListT is a monoid\!

--- Tactic1.hs
+++ Tactic1.hs
@@ -20,11 +20,15 @@ import Data.Monoid
 --Reader is commutative
 type Tactic c w = ListT (Reader c) w
 
+instance (Monad m) => Monoid (ListT m a) where
+    mempty = ListT $ return []
+    mappend t1 t2 = ListT $ (++) <$> (runListT t1) <*> (runListT t2)
+
 (.&) :: (w -> Tactic c x) -> (x -> Tactic c y) -> (w -> Tactic c y)
 (.&) = (>=>)
 
 (.|) :: (w -> Tactic c x) -> (w -> Tactic c x) -> (w -> Tactic c x)
-(.|) f g x = ListT $ (++) <$> (runListT $ f x) <*> (runListT $ g x)
+(.|) f g x = (f x) <> (g x)
 
 try :: (w -> Tactic c w) -> (w -> Tactic c w)
 try f = f .| return
